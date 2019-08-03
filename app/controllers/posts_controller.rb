041baf554fda20d1f65dcffe91d203691c07c979
mikiji13 <mikiji13kakinoki@gmail.com>
2019-08-03 16:15:32 +0900

    git commit -m "all"

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,12 @@
+class PostsController < ApplicationController
+  def index
+    @posts = Post.order("created_at DESC").page(params[:page]).per(6)
+  end
+
+  def new
+  end
+
+  def create
+    Post.create(name: "", image: "", text: "")
+  end
+end
