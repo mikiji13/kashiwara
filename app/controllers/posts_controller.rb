51a119737aef4a43703501bb2504a3a689ab7eb2
mikiji13 <mikiji13kakinoki@gmail.com>
2019-08-07 15:24:13 +0900

    routeの確立

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,10 +3,18 @@ class PostsController < ApplicationController
     @posts = Post.order("created_at DESC").page(params[:page]).per(6)
   end
 
+  def show
+    @posts = Post.find_by(id: params[:id])
+  end
+
   def new
   end
 
   def create
     Post.create(name: "", image: "", text: "")
   end
+
+
+
+
 end
