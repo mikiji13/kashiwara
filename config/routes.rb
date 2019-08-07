a8e8f53be5ebedd665856d3789b1e9ba53a0183f
mikiji13 <mikiji13kakinoki@gmail.com>
2019-08-07 17:25:22 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  get   '/'           =>  'posts#index'
+  root to: "posts#index"
+  get   'posts'           =>  'posts#index'
   get   "posts/:id"   =>  'posts#show'
   get   'posts/new'   =>  'posts#new'
   post  'posts'       =>  'posts#create'
