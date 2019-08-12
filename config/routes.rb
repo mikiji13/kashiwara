e230681158bac86752f88dd0e4982ef01ca2dee7
mikiji13 <mikiji13kakinoki@gmail.com>
2019-08-12 19:53:07 +0900

    a

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: "posts#index"
   get   'posts'       =>  'posts#index'
-  get   "posts/:id"   =>  'posts#show'
+  # get   "posts/:id"   =>  'posts#show'
   get   'posts/new'   =>  'posts#new'
   post  'posts'       =>  'posts#create'
 
